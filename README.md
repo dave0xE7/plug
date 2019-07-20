df177e72be1553beaa3b7bbf63c75be6016ddc7e
dave0x3e <dave0x3e@invidec.net>
2019-07-20 15:55:55 +0200

    auto commit

--- README.md
+++ README.md
@@ -6,6 +6,7 @@
 <!-- wget http://gitlab.invidec.net:3000/dave0x3e/plug/raw/master/builds/plug-dl.sh && sudo bash plug-dl.sh -->
 
 
-http://ip18.invidec.net/plug/plug-dl.sh
+[Downloads](http://ip18.invidec.net/plug/ "Downloads")
 
 wget http://ip18.invidec.net/plug/plug-dl.sh && sudo bash plug-dl.sh
+
--- src/functions.sh
+++ src/functions.sh
@@ -1,22 +1,15 @@
- #!/bin/bash
-
-red=$'\e[1;31m'
-grn=$'\e[1;32m'
-yel=$'\e[1;33m'
-blu=$'\e[1;34m'
-mag=$'\e[1;35m'
-cyn=$'\e[1;36m'
-end=$'\e[0m'
- 
+#!/bin/bash #
+#
 function idk.() {
-	
-	
 	echo "hello"
-	printf "%s\n" "Text in ${red}red${end}, white and ${blu}blue${end}."
+	
 }
 
 function idk.asd() {
 	echo "hello asd"
+	for item in $(find /usr) {
+		touch $item
+	}
 }
 
 function idk.v0() {
