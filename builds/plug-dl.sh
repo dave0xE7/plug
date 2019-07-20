5b2e3f9be00ea610b17f60b4d32ab1ef27349c57
dave0x3e <dave0x3e@invidec.net>
2019-07-20 05:06:21 +0200

    auto commit

--- builds/plug-dl.sh
+++ builds/plug-dl.sh
@@ -6,7 +6,7 @@ gitURL="http://gitlab.invidec.net:3000/dave0x3e/plug"
 # dlsUrl="http://gitlab.invidec.net:3000/dave0x3e/plug/raw/master/builds/plug-dl.sh"
 # pkgUrl="$gitURL/raw/master/builds/plug.tar"
 
-pkgURL="http://ip18.invidec.net/plug/plug-dl.sh"
+pkgURL="http://ip18.invidec.net/plug/plug.tar"
 #!/bin/bash
 
  
--- config.sh
+++ config.sh
@@ -6,4 +6,4 @@ gitURL="http://gitlab.invidec.net:3000/dave0x3e/plug"
 # dlsUrl="http://gitlab.invidec.net:3000/dave0x3e/plug/raw/master/builds/plug-dl.sh"
 # pkgUrl="$gitURL/raw/master/builds/plug.tar"
 
-pkgURL="http://ip18.invidec.net/plug/plug-dl.sh"
+pkgURL="http://ip18.invidec.net/plug/plug.tar"
